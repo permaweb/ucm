b1850dbfb9b9ed08f1b2e2ff03c7935b794475d2
twilson63 <tom@hyper.io>
2023-07-11 20:57:04 -0400

    buyback tests to support reverse dutch auction

--- src/cron/buyback.js
+++ src/cron/buyback.js
@@ -10,7 +10,7 @@ export async function buyback(state) {
     return state;
   }
 
-  const zAR_U = state.pairs.find(
+  let zAR_U = state.pairs.find(
     (p) => p.pair.includes(U) && p.pair.includes(SmartWeave.contract.id)
   );
 
@@ -20,12 +20,15 @@ export async function buyback(state) {
       orders: [],
       priceData: {},
     });
-    return state;
+    //return state;
+    zAR_U = state.pairs.find(
+      (p) => p.pair.includes(U) && p.pair.includes(SmartWeave.contract.id)
+    );
   }
 
-  if (zAR_U.orders.length === 0) {
-    return state;
-  }
+  // if (zAR_U.orders.length === 0) {
+  //   return state;
+  // }
 
   let uInventory = zAR_U.orders.reduce((a, o) => o.price * o.quantity + a, 0);
   let response = null
@@ -43,18 +46,21 @@ export async function buyback(state) {
   } else {
     // first look and see if there are any buy orders [U, PIXL] by this contract
     const orderToUpdate = await zAR_U.orders.find(o => o.creator === SmartWeave.contract.id)
+    
     if (orderToUpdate) {
-      orderToUpdate.originalQuantity = uBalance
-      orderToUpdate.quantity = uBalance
+      let price = Math.floor(orderToUpdate.price * 1.1)
+      orderToUpdate.originalQuantity = Math.floor(uBalance / price)
+      orderToUpdate.quantity = Math.floor(uBalance / price)
       orderToUpdate.price = Math.floor(orderToUpdate.price * 1.1)
     } else {
+      let price = zAR_U?.priceData?.vwap || 100
       response = await CreateOrder(state, {
         caller: SmartWeave.contract.id,
         input: {
           pair: [U, SmartWeave.contract.id],
-          qty: uBalance,
+          qty: Math.floor(uBalance / price),
           transaction: "INTERNAL_TRANSFER",
-          price: zAR_U?.priceData?.vwap || 1000
+          price
         },
       });
     }
@@ -62,15 +68,16 @@ export async function buyback(state) {
     
     // dutch auction
   }
-  
-
-  
-
-  // burn zAR
-  response.state.balances[SmartWeave.contract.id] = 0;
-  if (response.result.status === "success") {
-    return response.state;
+  if (response) {
+    // burn zAR
+    response.state.balances[SmartWeave.contract.id] = 0;
+    if (response.result.status === "success") {
+      return response.state;
+    } else {
+      return state;
+    }
   } else {
     return state;
   }
+  
 }
--- test/buy-back.test.js
+++ test/buy-back.test.js
@@ -5,7 +5,8 @@ import * as assert from "uvu/assert";
 // creates an sell order for $zAR. Burns the $zAR by setting its balance to zero
 const EYEBLOB_43 = "yfViHER2NCT7lEeR4nWKxG64ar3fKxagTP0OMfZLJmM";
 const U = "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw";
-test("buyback ", async () => {
+
+test("buyback U", async () => {
   globalThis.ContractAssert = function (expr, msg) {
     if (expr) {
       return null;
@@ -75,9 +76,148 @@ test("buyback ", async () => {
 
   const { buyback } = await import("../src/cron/buyback.js");
   const response = await buyback(state);
-  console.log(JSON.stringify(response, null, 2));
+  //console.log(JSON.stringify(response, null, 2));
   assert.equal(response?.pairs[0]?.priceData?.vwap, 100);
   assert.ok(true);
 });
+
+
+test("buyback U start dutch auction", async () => {
+  globalThis.ContractAssert = function (expr, msg) {
+    if (expr) {
+      return null;
+    } else {
+      throw new Error(msg);
+    }
+  };
+
+  globalThis.ContractError = function (msg) {
+    return new Error(msg);
+  };
+
+  globalThis.SmartWeave = {
+    block: {
+      height: 100000,
+    },
+    transaction: {
+      id: "W44dNBTBJAeNyb4Bo1IG1TI96VGLNah6m8sy9HUKu5Y",
+    },
+    contract: {
+      id: "AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI",
+    },
+    contracts: {
+      write(id, input) {
+        //console.log(id, input);
+        return Promise.resolve({ type: "ok" });
+      },
+      readContractState(id) {
+        if (id === U) {
+          return Promise.resolve({
+            balances: {
+              "AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI": 10000,
+            },
+          });
+        }
+        //console.log('readState', id)
+        return Promise.resolve({});
+      },
+    },
+  };
+  const state = {
+    U: "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw",
+    balances: {},
+    pairs: [],
+    claimable: [],
+    name: "UCM",
+    ticker: "PIXL",
+    recentRewards: {},
+    lastReward: 0,
+  };
+
+  const { buyback } = await import("../src/cron/buyback.js");
+  const response = await buyback(state);
+  console.log(response.pairs[0])
+  assert.equal(response?.pairs[0]?.orders[0]?.price, 100);
+  assert.ok(true);
+})
+
+test("buyback U increment dutch auction", async () => {
+  globalThis.ContractAssert = function (expr, msg) {
+    if (expr) {
+      return null;
+    } else {
+      throw new Error(msg);
+    }
+  };
+
+  globalThis.ContractError = function (msg) {
+    return new Error(msg);
+  };
+
+  globalThis.SmartWeave = {
+    block: {
+      height: 100000,
+    },
+    transaction: {
+      id: "W44dNBTBJAeNyb4Bo1IG1TI96VGLNah6m8sy9HUKu5Y",
+    },
+    contract: {
+      id: "AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI",
+    },
+    contracts: {
+      write(id, input) {
+        //console.log(id, input);
+        return Promise.resolve({ type: "ok" });
+      },
+      readContractState(id) {
+        if (id === U) {
+          return Promise.resolve({
+            balances: {
+              "AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI": 20000,
+            },
+          });
+        }
+        //console.log('readState', id)
+        return Promise.resolve({});
+      },
+    },
+  };
+
+  const state = {
+    U: "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw",
+    balances: {},
+    pairs: [
+      {
+        pair: [
+          'AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI',
+          'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw'
+        ],
+        orders: [
+          {
+            id: 'W44dNBTBJAeNyb4Bo1IG1TI96VGLNah6m8sy9HUKu5Y',
+            transfer: 'INTERNAL_TRANSFER',
+            creator: 'AHrcXuowqLwX-EzPhks-Hla3BY7gPMc9XpYDi2sHSCI',
+            token: 'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw',
+            price: 100,
+            quantity: 100,
+            originalQuantity: 100
+          }
+        ],
+        priceData: undefined
+      }
+    ],
+    claimable: [],
+    name: "UCM",
+    ticker: "PIXL",
+    recentRewards: {},
+    lastReward: 0,
+  };
+
+  const { buyback } = await import("../src/cron/buyback.js");
+  const response = await buyback(state);
+  //console.log(response.pairs[0])
+  assert.equal(response?.pairs[0]?.orders[0]?.price, 110);
+  assert.ok(true);
+})
 
 test.run();
