1c40c61bcf0af861b2c0af8aa63485a79d82367d
Tom Wilson <tom@hyper.io>
2023-08-27 19:51:26 +0000

    fix: this pr corrects the issue of selling pixl via allow/claim flow

---
+++ test/sell-pixl.test.js
@@ -0,0 +1,93 @@
+// sell pixl
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
+globalThis.ContractAssert = function (expr, msg) {
+  if (!expr) {
+    throw new Error(msg);
+  }
+};
+
+test('sell pixl', async () => {
+  globalThis.SmartWeave = setupGlobalState()
+  const state = setupContractState()
+  const allowAction = setupAllowAction()
+  const createOrderAction = setupCreateOrderAction()
+
+  const { handle } = await import('../src/index.js')
+  assert.equal(state.balances["jnbRhoH3JGTdRz0Y9X-gh-eosrbIpdxs58DPTtlOVE8"], 1000 * 1e6)
+  // allow
+  const result = await handle(state, allowAction)
+  // console.log(JSON.stringify(result.state, null, 2))
+  // createOrder
+  const result2 = await handle(result.state, createOrderAction)
+  // verify createOrder is created
+  // console.log(JSON.stringify(result2, null, 2))
+  // verify balance is correct
+  assert.equal(result2.state.balances["hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8"], 1000 * 1e6)
+  assert.equal(result2.state.balances["jnbRhoH3JGTdRz0Y9X-gh-eosrbIpdxs58DPTtlOVE8"], 0)
+  assert.ok(true)
+
+})
+
+test.run()
+
+function setupGlobalState() {
+  return {
+    block: {
+      height: 1209775,
+    },
+    transaction: {
+      id: "oeYUgBDGBql5-ik4DJ5cDvacwmYe03jx6A5pQK7DEBw",
+    },
+    contract: {
+      id: "hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8",
+    },
+  };
+}
+
+function setupContractState() {
+  return {
+    U: "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw",
+    streaks: {},
+    balances: {
+      "jnbRhoH3JGTdRz0Y9X-gh-eosrbIpdxs58DPTtlOVE8": 1000 * 1e6
+    },
+    claimable: [],
+    name: 'PIXL',
+    ticker: 'PIXL',
+    pairs: [
+      {
+        pair: ["hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8", "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw"],
+        orders: [],
+        pricedata: {},
+      },
+    ],
+    recentRewards: {},
+    lastReward: 0,
+  }
+}
+
+function setupAllowAction() {
+  return {
+    caller: "jnbRhoH3JGTdRz0Y9X-gh-eosrbIpdxs58DPTtlOVE8",
+    input: {
+      function: 'allow',
+      target: "hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8",
+      qty: 1000 * 1e6,
+    }
+  }
+}
+
+function setupCreateOrderAction() {
+  return {
+    caller: 'jnbRhoH3JGTdRz0Y9X-gh-eosrbIpdxs58DPTtlOVE8',
+    input: {
+      function: 'createOrder',
+      pair: ["hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8", "KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw"],
+      qty: 1000 * 1e6,
+      price: .01,
+      txID: "oeYUgBDGBql5-ik4DJ5cDvacwmYe03jx6A5pQK7DEBw",
+    }
+  }
+}
