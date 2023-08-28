6477989d8c98005e16f7dbd0bc91f9a138d6ef25
Tom Wilson <tom@hyper.io>
2023-08-28 15:29:27 +0000

    used evolve to deploy fix

--- scripts/evolve.js
+++ scripts/evolve.js
@@ -7,13 +7,14 @@ const jwk = JSON.parse(fs.readFileSync('./wallet.json', 'utf-8'))
 const signer = new ArweaveSigner(jwk)
 //const ucm = 'XW_z0WhM5PsVD-nmyNm1pCK1za9uysu1vco1HS8DpIo'
 //const ucm = 'V6fwgkTtbRJVu_yKGJM2RQ6bHte35_g0Sj2C_BIdScs'
-const ucm = 'IOtejAGHWiiwz4HuVDagKUldWDlhsaTnmXQStSqcsrw'
+const ucm = 'tfalT8Z-88riNtoXdF5ldaBtmsfcSmbMqWLh2DHJIbg'
 
 async function main() {
   const src = fs.readFileSync('./dist/index.js', 'utf-8')
   const tx = await warp.createSource({ src }, signer)
-  const newSrcId = await warp.saveSource(tx)
-  console.log(newSrcId)
+  //const newSrcId = await warp.saveSource(tx)
+  //console.log(newSrcId)
+  const newSrcId = 'qOd7mNAJdju9PxtsRJbel4Zu3xYgEwUbxW8U14czjD8'
   const result = await warp.contract(ucm).connect(signer).evolve(newSrcId)
   console.log(result)
 }
