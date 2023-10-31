831f1b898c25e78a636e94ff953c1bde507fcf77
Tom Wilson <tom@hyper.io>
2023-10-31 18:59:07 +0000

    fix: setup for evolve #5

--- scripts/evolve.js
+++ scripts/evolve.js
@@ -10,13 +10,13 @@ const signer = new ArweaveSigner(jwk)
 const ucm = 'tfalT8Z-88riNtoXdF5ldaBtmsfcSmbMqWLh2DHJIbg'
 
 async function main() {
-  const src = fs.readFileSync('./dist/index.js', 'utf-8')
-  const tx = await warp.createSource({ src }, signer)
-  const newSrcId = await warp.saveSource(tx)
-  console.log(newSrcId)
-  // const newSrcId = '8kPgNMm7dZUVk93T7wq05otEy1oDNqZhyD3L7WrcMTY'
-  // const result = await warp.contract(ucm).connect(signer).evolve(newSrcId)
-  // console.log(result)
+  // const src = fs.readFileSync('./dist/index.js', 'utf-8')
+  // const tx = await warp.createSource({ src }, signer)
+  // const newSrcId = await warp.saveSource(tx)
+  // console.log(newSrcId)
+  const newSrcId = 'DXc7dRQe8AZRPNqHzjewWWeT7dUeEQxBEEYOHlomp2g'
+  const result = await warp.contract(ucm).connect(signer).evolve(newSrcId)
+  console.log(result)
 }
 
 main()
